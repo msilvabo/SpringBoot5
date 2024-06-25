af3b43654d10c46a00b2603747c6734248e81e3f
Moises Silva Cardenas <moises.silva@et.bo>
2024-06-25 08:27:21 -0400

    first commit

---
+++ src/main/java/com/moises/curso/springboot/manager/error/exceptions/ExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.moises.curso.springboot.manager.error.exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moises/curso/springboot/manager/error/exceptions/ExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moises.curso.springboot.manager.error.exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
